d1a08f0f4ce3bf2e1debb9bf06716b4e03cf1e79
Randy Taylor <randyisbusy@gmail.com>
2019-09-29 16:33:05 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ActiveRecord::Base
+  def change
+      create_table :students do |t|
+        t.string :name
+        t.integer :grade
+        t.integer :birthdate
+      end
+    end
 end
