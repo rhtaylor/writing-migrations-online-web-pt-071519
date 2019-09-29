f80ce21dfaa3952bdcfa8b416e0ebf1ef6fe4142
Randy Taylor <randyisbusy@gmail.com>
2019-09-29 16:31:48 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,7 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
 
   def change
 
-   create_table :artists do |t|
+   create_table :students do |t|
      t.string :name
      t.string :genre
      t.integer :age
