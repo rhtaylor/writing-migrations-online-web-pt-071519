8c9c16101d8310e506c8988a0a09d1bb61387228
Randy Taylor <randyisbusy@gmail.com>
2019-09-29 16:27:29 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
   def change
       create_table :students do |t|
         t.string :name
-        t.integer :grade 
+        t.integer :grade
         t.integer :birthdate
       end
     end
